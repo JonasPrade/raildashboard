f8905d64a9fa9ea1a9341805bcd106a827548d42
JonasPrade <j.prade@gmx.de>
2026-02-19 14:57:59 +0100

    finished fusion of merg repositories

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    envPrefix: ["VITE_", "REACT_APP_"]
+    envPrefix: ["VITE_", "REACT_APP_"],
+    test: {
+        environment: "jsdom",
+        globals: true,
+    },
 });
